fb214e35f791e99bc1ba281e44b08b086165050b
hoshino <2683442914@qq.com>
2019-02-15 23:24:45 +0800

    咕咕咕

--- 35_CopyComplexList/ComplexList.cpp
+++ 35_CopyComplexList/ComplexList.cpp
@@ -15,7 +15,7 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 // 面试题35：复杂链表的复制
 // 题目：请实现函数ComplexListNode* Clone(ComplexListNode* pHead)，复
 // 制一个复杂链表。在复杂链表中，每个结点除了有一个m_pNext指针指向下一个
-// 结点外，还有一个m_pSibling 指向链表中的任意结点或者nullptr。 
+// 结点外，还有一个m_pSibling 指向链表中的任意结点或者nullptr。  
 
 #include <cstdio>
 #include "ComplexList.h"
